0ae85fb68b17bf74bc9f2b19befbb25e8f8af5b2
RAFAEL LEON ESTRADA <rle16@hotmail.com>
2022-03-09 15:21:19 -0600

    Creacion de forms

---
+++ Forms/FormReservacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Missushi.Forms
+{
+    public partial class FormReservacion : Form
+    {
+        public FormReservacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
