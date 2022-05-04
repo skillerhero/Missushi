f099ba29b20c54bc406614b6e18fb3dcafb44192
Rafael Leon <48114839+skillerhero@users.noreply.github.com>
2022-05-03 22:29:57 -0500

    ,

--- Clases/Globales.cs
+++ Clases/Globales.cs
@@ -42,12 +42,10 @@ namespace Missushi.Clases {
             string FileName = string.Format("{0}Resources\\Gabriola.ttf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\..\")));
             pfc.AddFontFile(FileName);
             letraGabriola = new Font(pfc.Families[0], 20.25F, FontStyle.Regular);
-            letraGabriola = new Font("Gabriola", 20.25F, FontStyle.Regular);
             pfc = new PrivateFontCollection();
             FileName = string.Format("{0}Resources\\CenturyGothic.ttf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\..\")));
             pfc.AddFontFile(FileName);
             letraCenturyGothic = new Font(pfc.Families[0], 12F, FontStyle.Regular, GraphicsUnit.Point);
-            letraCenturyGothic = new Font("Century Gothic", 20.25F, FontStyle.Regular);
         }
 
         public static void transition() {
