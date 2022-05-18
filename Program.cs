6a828c302b6e62204dd10291a297f9e1c063662c
RAFAEL LEON ESTRADA <rle16@hotmail.com>
2022-05-18 12:29:35 -0500

    Últimos cambios

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
 using Missushi.Clases;
 using Missushi.Funciones;
 
-namespace Missushi
-{
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+namespace Missushi{
+    internal static class Program{
         [STAThread]
-        static void Main()
-        {
+        static void Main(){
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
